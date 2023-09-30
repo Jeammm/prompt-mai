d87aa5e700ad86579bd1a68b566859560e849482
Jeammm <87964996+Jeammm@users.noreply.github.com>
2023-10-01 00:12:27 +0700

    add header spacing

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text_center">
-        Discover and Share
+        Discover and Share {" "}
         <br className="max-md:hidden" />
         <span className="orange_gradient">AI-Powered Prompt</span>
       </h1>
