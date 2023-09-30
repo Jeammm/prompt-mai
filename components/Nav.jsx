bf75ccc45f91f208f1b01898a3820b1745dd86f8
Jeammm <87964996+Jeammm@users.noreply.github.com>
2023-10-01 00:08:43 +0700

    style sign in btn

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -73,6 +73,7 @@ const Nav = () => {
                   type="button"
                   key={provider.name}
                   onClick={() => signIn(provider.id)}
+                  className="black_btn"
                 >
                   Sign In
                 </button>
@@ -126,6 +127,7 @@ const Nav = () => {
                   type="button"
                   key={provider.name}
                   onClick={() => signIn(provider.id)}
+                  className="black_btn"
                 >
                   Sign In
                 </button>
